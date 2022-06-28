810ae8d34d3b9465929dec730de1aa1fd32f3a7e
hychim <hychim@protonmail.com>
2022-06-28 13:24:16 +0800

    Update pdb2crosslink.py

--- 6esq/data/pdb2crosslink.py
+++ 6esq/data/pdb2crosslink.py
@@ -29,20 +29,22 @@ model = structure[0]
 chainA = model['A']
 chainC = model['C']
 
+cldb = "Protein 1,Protein 2,Residue 1,Residue 2,UniqueID,Score \n"
+
 # loop through all residue in chain
 for residue1 in chainA:
-    for residue2 in chainA:
-        if residue1 != residue2:
-            # compute distance between CA atoms
-            try:
-                distance = residue1['CA'] - residue2['CA']
-            except KeyError:
+    for residue2 in chainC:
+        # compute distance between CA atoms
+        try:
+            distance = residue1['CA'] - residue2['CA']
+        except KeyError:
                 ## no CA atom, e.g. for H_NAG
                 continue
-            if distance < 6:
-                print(residue1, residue2, distance)
-        # stop after first residue
-        break
+        if distance < 6:
+            #print(chainA, residue1, chainC, residue2, distance)
+            cldb += f"ProtA,ProtB,{residue1.get_id()[1]},{residue2.get_id()[1]},,1.0 \n"
+
+print(cldb)
 
 ### Cross linking data generation
 xldb='''Protein 1,Protein 2,Residue 1,Residue 2,UniqueID,Score
